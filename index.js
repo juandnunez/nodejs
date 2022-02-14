c261432af31c3844a65eaef9cfc06dbd885ef179
juandnunez <juan.d.nunez.aljure@gmail.com>
2022-02-13 19:12:00 -0500

    todo list on nodejs :)

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
-
 const app = express();
-
-
-app.listen(4444)
-
 app.use("/homepage", (req, res)=> {
     res.send("homepage setup on GET")
 })
+
+app.set("view engine", "ejs");
+
+app.use("/static", express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req,res) => {
+    res.render("todo.ejs");
+});
+
+app.listen(4444)
