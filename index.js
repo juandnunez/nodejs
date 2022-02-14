bda62b74e19c9b94e197e01d4c001a78120a3952
juandnunez <juan.d.nunez.aljure@gmail.com>
2022-02-13 19:13:25 -0500

    console log record

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.set("view engine", "ejs");
 app.get("/", (req,res) => {
     res.render("todo.ejs");
 });
-
-app.listen(4444)
+app.listen(4444);
+console.log("this is for learning and fun purposes, taken from https://medium.com/@diogo.fg.pinheiro/simple-to-do-list-app-with-node-js-and-mongodb-chapter-1-c645c7a27583")
